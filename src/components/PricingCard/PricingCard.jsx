026d870f2583bba8872bfa31079e903922ca0de3
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-05-02 19:21:56 +0600

    completed the pricing section

--- src/components/PricingCard/PricingCard.jsx
+++ src/components/PricingCard/PricingCard.jsx
@@ -1,19 +1,21 @@
 const PricingCard = ({ pricing }) => {
-  const { name, shortDes, price, period, features } = pricing;
+  const { name, shortDes, price, period, features, buttonText, tagName } = pricing;
   return (
     <div className="card shadow-sm border border-gray-200 relative rounded-2xl">
       <div className="card-body">
-        <span className="badge badge-xs badge-warning">Most Popular</span>
+        {
+            tagName === "Most Popular" && <span className="absolute -top-3 left-1/2 -translate-x-1/2 badge badge-md rounded-full font-semibold bg-[#FEF3C6] text-[#BB4D00] whitespace-nowrap">Most Popular</span>
+        }
         <div className="">
-          <h2 className="text-3xl font-bold">{name}</h2>
-          <p>{shortDes}</p>
-          <span className="text-xl">
-            ${price}/{period}
+          <h2 className="text-2xl font-bold">{name}</h2>
+          <p className="text-[#627382] mb-3">{shortDes}</p>
+          <span className="text-3xl font-bold">
+            ${price}<span className="text-[16px] font-normal text-[#627382]">/{period}</span>
           </span>
         </div>
-        <ul className="mt-6 flex flex-col gap-2 text-xs">
+        <ul className="mt-3 flex flex-col gap-2 text-xs mb-2">
           {features.map((feature, index) => (
-            <li key={index}>
+            <li className="text-[#627382]" key={index}>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="size-4 me-2 inline-block text-success"
@@ -32,8 +34,10 @@ const PricingCard = ({ pricing }) => {
             </li>
           ))}
         </ul>
-        <div className="mt-6">
-          <button className="btn btn-primary btn-block">Subscribe</button>
+        <div className="mt-auto">
+          <button className="w-full btn rounded-full bg-linear-to-r from-[#4F39F6] to-[#9514FA] px-6 text-white hover:from-[#9514FA] hover:to-[#4F39F6] border-none font-bold">
+            {buttonText}
+          </button>
         </div>
       </div>
     </div>

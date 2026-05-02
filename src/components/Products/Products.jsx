f76957c1f2fc1f1efb627a1e3bedf867a4bf68fc
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-05-02 14:03:21 +0600

    made the cards dynamically

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,13 +1,17 @@
 import { use } from "react";
+import Product from "../Product/Product";
 
 const Products = ({ productItems }) => {
 
     const products = use(productItems);
-    console.log(products);
     
     return (
-        <div>
-            Products
+        <div className="max-w-7xl mx-auto">
+            <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-7">
+                {
+                    products.map(product => <Product key={product.id} product={product} />)
+                }
+            </div>
         </div>
     );
 };

4251b23324515b080f2c6ac60c6847463a6ff66a
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-05-03 09:58:26 +0600

    made the product card buy now button clickable

--- src/components/PremiumTools/PremiumTools.jsx
+++ src/components/PremiumTools/PremiumTools.jsx
@@ -26,7 +26,7 @@ const PremiumTools = ({ products, setProducts }) => {
           className={
             products === "carts" ? "w-1/2 btn rounded-full bg-linear-to-r from-[#4F39F6] to-[#9514FA] px-6 text-white hover:from-[#9514FA] hover:to-[#4F39F6] shadow-md border-none shadow-[#4F39F6]/90 font-bold" : "tab w-1/2"
           }
-          aria-label="Cart(0)"
+          aria-label="Cart (0)"
           onClick={() => setProducts("carts")}
         />
       </div>
--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import I1 from "../../assets/products/i-1.png";
 import I2 from "../../assets/products/i-2.png";
 import I3 from "../../assets/products/i-3.png";
@@ -7,25 +8,33 @@ import I6 from "../../assets/products/i-6.png";
 import I7 from "../../assets/products/i-7.png";
 import I8 from "../../assets/products/i-8.png";
 import I9 from "../../assets/products/i-9.png";
+import { HiOutlineCheck } from "react-icons/hi";
 
 const images = [I1, I2, I3, I4, I5, I6, I7, I8, I9];
 
 const Product = ({ product }) => {
+
+  const [click, setClick] = useState(false);
+  const handleClick = () => {
+    setClick(!click);
+  }
+
   const { id, name, description, price, period, tag, tagType, features } =
     product;
 
   return (
     <div className="card shadow-sm border border-gray-200 relative rounded-2xl hover:-translate-y-2 transition-all duration-300 hover:border-gray-300">
       <div className="card-body">
-        <span className={`badge badge-md font-medium rounded-full ${tagType === "best-seller" ? "bg-[#FEF3C6] text-[#BB4D00]" : tagType === "popular" ? "badge-soft badge-primary" : "bg-[#DBFCE7] text-green-600"} absolute top-2 right-2 capitalize`}>
+        <span
+          className={`badge badge-md font-medium rounded-full ${tagType === "best-seller" ? "bg-[#FEF3C6] text-[#BB4D00]" : tagType === "popular" ? "badge-soft badge-primary" : "bg-[#DBFCE7] text-green-600"} absolute top-2 right-2 capitalize`}
+        >
           {tag}
         </span>
         <div className="w-14 h-14 border border-gray-200 flex justify-center items-center rounded-full">
           <img className="w-8" src={images[id - 1]} alt={name} />
         </div>
-        <div className="">
-          <h2 className="text-2xl font-bold">{name}</h2>
-        </div>
+        <h2 className="text-2xl font-bold">{name}</h2>
+
         <p className="text-xl font-bold">
           ${price}
           <span className="text-[14px] text-gray-500 capitalize font-normal">
@@ -34,7 +43,7 @@ const Product = ({ product }) => {
         </p>
         <p className="text-gray-500">{description}</p>
         <ul className="mt-2 flex flex-col gap-2 text-xs">
-          {features.map((feature,index) => (
+          {features.map((feature, index) => (
             <li key={index}>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -50,15 +59,17 @@ const Product = ({ product }) => {
                   d="M5 13l4 4L19 7"
                 />
               </svg>
-              <span className="text-gray-500 font-medium text-[15px]">{feature}</span>
+              <span className="text-gray-500 font-medium text-[15px]">
+                {feature}
+              </span>
             </li>
           ))}
         </ul>
-        <div className="mt-3">
-          <button className="w-full btn rounded-full bg-linear-to-r from-[#4F39F6] to-[#9514FA] px-6 text-white hover:from-[#9514FA] hover:to-[#4F39F6] border-none font-bold">
-            Buy Now
-          </button>
-        </div>
+        <button onClick={handleClick} className={`w-full btn rounded-full ${click ? "bg-green-600 text-white" : "bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-white hover:from-[#9514FA] hover:to-[#4F39F6]"} border-none font-bold mt-3`}>
+          {
+            click ? <span className="flex items-center gap-2"><HiOutlineCheck className="text-xl " />Added to Cart</span> : "Buy Now"
+          }
+        </button>
       </div>
     </div>
   );
